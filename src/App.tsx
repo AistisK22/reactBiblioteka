2eeeb5e83aa578f974ba96e7daec49279b879380
AistisK22 <aiskai1@ktu.lt>
2025-03-08 17:35:31 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Button from './Button'
+import Card from './Card'
+
+function App() {
+
+  return (
+      <>
+          <Button label="Click" variant={''} size={''} />
+          <Card
+              title="Click"
+              shadow
+              includeImage
+              imageUrl={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.devotional-reflections-from-the-bible.com%2Fimages%2F2020RF21.jpg&f=1&nofb=1&ipt=f85f4710ef77f419d33797e33d84d509ec8d31c497965449171ba3166b0d0b6a&ipo=images'}
+              body={'teteetetetetetetteteetetetetetetteteetetetetet'+
+                  'etteteetetetetetetteteetetetetetetteteetetetetetetteteetetetetetet'
+                  + 'teteetetetetetet'
+              }
+          />
+      </>
+  )
+}
+
+export default App
---
+++ src/Card.tsx
@@ -0,0 +1,49 @@
+interface CardProps {
+    title?: string;
+    body?: string;
+    color?: string;
+    textColor?: string;
+    shadow?: boolean;
+    includeImage?: boolean;
+    imageUrl?: string;
+}
+
+const Card = ({ title, body, color, textColor, shadow, includeImage, imageUrl }: CardProps) => {
+    return (
+        <div
+            className="card"
+            style={{
+                color: textColor ? textColor : 'black',
+                backgroundColor: color ? color : 'white',
+                width: "auto",
+                minWidth: "100px",
+                maxWidth: "960px",
+                height: "auto",
+                margin: '1rem 0',
+                fontFamily: 'Roboto',
+                borderRadius: '16px',
+                border: 'solid 1px gray',
+                boxShadow: shadow ? 'rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px' : 'none'
+            }}
+        >
+            {
+                includeImage
+                &&
+                <img
+                    className='card-image'
+                    src={imageUrl}
+                />
+            }
+            <div style={{ padding: '1rem' }}>
+                <h2 style={{ margin: "0", wordWrap: 'break-word' }}>
+                    {title}
+                </h2>
+                <div style={{ fontSize: "large", marginTop: "1rem", wordWrap:'break-word' }}>
+                    {body}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
